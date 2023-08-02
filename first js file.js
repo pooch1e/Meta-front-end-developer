bc206e374f4a900958fa40bdad61fc2aae4595a2
pooch1e <joelnskram@gmail.com>
2023-08-02 14:16:50 +0100

    trying first exercises in module

---
+++ first js file.js
@@ -0,0 +1,15 @@
+/* first test
+var score= 8;
+if (score > 0 && score < 10) {
+    console.log("Mid-Level skills: True")
+    
+} else {
+    console.log("not yet")
+}
+*/
+
+//second test exercise: advanced use operators
+var timeRemaining=0;
+var energy=10;
+console.log("Game over: ", timeRemaining == 0 || energy == 0);
+
